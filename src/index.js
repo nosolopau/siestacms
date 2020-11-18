22f4030294280bd95948a200de16203ab97b2c3c
Pablo Torrecilla <pau@nosolopau.com>
2020-11-17 20:54:08 -0800

    Use S3 (terrible code, WIP)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const parser = require('fast-xml-parser');
 const express = require('express');
 const fs = require('fs');
 const expressLayouts = require('express-ejs-layouts');
+const {S3} = require("aws-sdk");
+const s3 = new S3();
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -16,31 +18,48 @@ app.use(expressLayouts);
 const postsFolder = './posts/';
 const Post = require("./models/post.js");
 
-app.get('/', (req, res) => {
-    let files = fs.readdirSync(postsFolder);   
-    
-    let posts = [];
-    for(let file of files) {
-        const post = loadPost(file)
-        posts.push(post);
+app.get('/', async (req, res, next) => {
+    try {
+        loadPosts().then((posts) => {
+            res.render('index', {posts: posts, layout: 'layout'});
+        });
+    } catch(e) {
+        handleError(e, res);
     }
-
-    res.render('index', {posts: posts, layout: 'layout'});
 })
 
-app.get('/posts/:file', (req, res) => {
+app.get('/posts/:file', async (req, res, next) => {
     try {
-        const post = loadPost(req.params.file);
-        res.render('post', {post: post, layout: 'layout'})
+        loadPost(req.params.file).then((post) => {
+            res.render('post', {post: post, layout: 'layout'})
+        });
     }
     catch(e) {
         handleError(e, res);
     }
 })
 
-function loadPost(file){
-    var rawPostData = fs.readFileSync(postsFolder + file).toString();
-    return new Post(file, parser.parse(rawPostData, {stopNodes: "body"}));
+async function loadPost(file){
+    if(process.env.USE_S3 == "true") {
+        return await loadPostFromS3(file)
+    } 
+    else {
+        var rawPostData = fs.readFileSync(postsFolder + file).toString();
+        return new Post(file, parser.parse(rawPostData, {stopNodes: "body"}));
+    }
+}
+
+async function loadPostFromS3(objectKey) {
+    try {
+        const params = {
+            Bucket: process.env.POSTS_S3_BUCKET,
+            Key: objectKey 
+        }
+        let rawPostData = await s3.getObject(params).promise();
+        return new Post(objectKey, parser.parse(rawPostData.Body.toString('utf-8'), {stopNodes: "body"}));
+    } catch (e) {
+        console.log(`Could not retrieve file from S3: ${e.message}`)
+    }
 }
 
 function handleError(e, res) {
@@ -54,6 +73,40 @@ function handleError(e, res) {
 
     res.status(code).redirect('/');
 }
+
+ async function loadPosts() {
+    let posts = [];
+
+    if(process.env.USE_S3 == "true") {
+        let params = {
+            Bucket: process.env.POSTS_S3_BUCKET
+        };
+        let files = await getFilesRecursively(params);
+        for(let file of files) {
+            const post = await loadPostFromS3(file.Key);
+            posts.push(post);
+        }
+    } else {
+        let files = fs.readdirSync(postsFolder);   
+        for(let file of files) {
+            const post = loadPost(file)
+            posts.push(post);
+        }
+    }
+    return posts;
+}
+
+async function getFilesRecursively(param) {
+    let result = await s3.listObjectsV2(param).promise();
+
+    if(!result.IsTruncated) {
+        return result.Contents;
+    } else {
+        param.ContinuationToken = result.NextContinuationToken;
+        return result.Contents.concat(await getFilesRecursively(param));
+    }
+}
+
 
 module.exports.handler = serverless(app);
 
